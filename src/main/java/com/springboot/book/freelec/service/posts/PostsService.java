85b7ad0cd1ec018ca3fbbaa73d9eff48d1524182
jjeongin <jeonginlee0304@gmail.com>
2021-07-19 17:36:02 +0900

    Applied template engine, added blog post delete function

--- src/main/java/com/springboot/book/freelec/service/posts/PostsService.java
+++ src/main/java/com/springboot/book/freelec/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package com.springboot.book.freelec.service.posts;
 import com.springboot.book.freelec.domain.posts.Posts;
 import com.springboot.book.freelec.domain.posts.PostsRepository;
+import com.springboot.book.freelec.web.dto.PostsListResponseDto;
 import com.springboot.book.freelec.web.dto.PostsResponseDto;
 import com.springboot.book.freelec.web.dto.PostsSaveRequestDto;
 import com.springboot.book.freelec.web.dto.PostsUpdateRequestDto;
@@ -8,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,18 @@ public class PostsService {
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No post exists. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No post exists. id=" + id));
+        postsRepository.delete(posts);
     }
 }
