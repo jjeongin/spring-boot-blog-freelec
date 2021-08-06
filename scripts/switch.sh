ad937db41df2383a23226e16f8e81e19428b3bc6
jjeongin <jeonginlee0304@gmail.com>
2021-08-06 17:39:24 +0900

    Revised command line messages

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -7,7 +7,7 @@ source ${ABSDIR}/profile.sh
 function switch_proxy() {
   IDLE_PORT=$(find_idle_port)
 
-  echo "> Switching port: $IDLE_PORT"
+  echo "> Switching to port: $IDLE_PORT"
   echo "> Switch Port"
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
